7f9420bd226ecdf1a939a94ae655edd7961390a4
PastFantasy1640 <pastfantasy1640@gmail.com>
2016-10-28 16:34:24 +0900

    WEB-INFがそもそも存在するかチェックを追加

--- jcopy.sh
+++ jcopy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -d WEB-INF ]; then
+	echo "コピー開始"
+else
+	echo "WEB-INFフォルダがカレントディレクトリにないよ"
+	echo "Copy Failed."
+	exit -1
+fi
+
 if [ -d /usr/local/MyApp/WEB-INF ]; then
 	cp -ir WEB-INF /usr/local/MyApp
 else
