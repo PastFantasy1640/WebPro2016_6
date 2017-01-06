83c4cb5502da3c84a7bd76122f2311c5321afb62
Boston20161021 <Boston20161021@gmail.com>
2017-01-06 13:26:37 +0900

    Add files via upload

---
+++ sites/circle/universities_dump.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.16, for osx10.12 (x86_64)
+--
+-- Host: localhost    Database: webpro_db
+-- ------------------------------------------------------
+-- Server version	5.7.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `universities`
+--
+
+LOCK TABLES `universities` WRITE;
+/*!40000 ALTER TABLE `universities` DISABLE KEYS */;
+INSERT INTO `universities` VALUES (1,'九州工業大学情報工学部',40,'き'),(2,'九州工業大学工学部',40,'き'),(3,'大分大学経済学部',44,'お'),(4,'宮崎大学教育学部',45,'み'),(5,'鹿児島大学歯学部',46,'か'),(6,'熊本大学理学部',43,'く'),(7,'佐賀大学理工学部',41,'さ'),(8,'長崎大学薬学部',42,'な'),(9,'愛知大学',23,'あ'),(10,'茨城大学',8,'い'),(11,'横浜国立大学',14,'よ'),(12,'東京大学',13,'と'),(13,'一橋大学',13,'ひ'),(14,'東北大学',4,'と'),(15,'埼玉大学',11,'さ'),(16,'東京医科歯科大学',13,'と'),(17,'山梨大学',19,'や'),(18,'東京芸術大学',13,'と'),(19,'群馬大学',10,'く'),(20,'筑波大学',8,'つ'),(21,'電気通信大学',13,'て'),(22,'お茶の水女子大学',13,'お'),(23,'岩手大学',3,'い'),(24,'大阪教育大学',27,'お'),(25,'弘前大学',2,'ひ'),(26,'東京工業大学',13,'と'),(27,'東京農工大学',13,'と'),(28,'長崎大学',42,'な'),(29,'岐阜大学',21,'き'),(30,'滋賀大学',25,'し'),(31,'小樽商科大学',1,'お'),(32,'筑波技術大学',8,'つ'),(33,'佐賀大学',41,'さ'),(34,'宮崎大学',45,'み'),(35,'神戸大学',28,'こ'),(36,'山形大学',6,'や'),(37,'琉球大学',47,'り'),(38,'東京外国語大学',13,'と'),(39,'広島大学',34,'ひ'),(40,'大分大学',44,'お'),(41,'北海道大学',1,'ほ'),(42,'信州大学',20,'し'),(43,'東京海洋大学',13,'と'),(44,'山口大学',35,'や'),(45,'京都工芸繊維大学',26,'き'),(46,'新潟大学',15,'に'),(47,'鳥取大学',31,'と'),(48,'三重大学',24,'み'),(49,'名古屋大学',23,'な'),(50,'千葉大学',12,'ち'),(51,'奈良女子大学',29,'な'),(52,'旭川医科大学',1,'あ'),(53,'岡山大学',33,'お'),(54,'豊橋技術科学大学',23,'と'),(55,'宇都宮大学',9,'う'),(56,'室蘭工業大学',1,'む'),(57,'鹿児島大学',46,'か'),(58,'兵庫教育大学',28,'ひ'),(59,'金沢大学',17,'か'),(60,'熊本大学',43,'く'),(61,'宮城教育大学',4,'み'),(62,'早稲田大学',13,'わ');
+/*!40000 ALTER TABLE `universities` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-06 13:24:20
